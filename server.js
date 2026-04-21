ac78b670e94a6ca713dcafbd4fba6f3cf12ee590
Dhanush <ddhanush5761@gmail.com>
2026-04-21 20:40:46 +0530

    changes in prediction bill calculation

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.get("/predict/:id", async (req, res) => {
       kwhPerHour = current.total_energy / diffHours;
     }
 
-    const kwhPerDay = kwhPerHour * 24;
+    const kwhPerDay = (live.total_energy) * 24;
     const kwh7      = kwhPerDay * 7;
     const kwh30     = kwhPerDay * 30;
 
